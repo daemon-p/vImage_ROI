30b0756bbcf0ad3d9dea49cf67f281f3bb1fedd3
Dumbass <dumbass.cn@outlook.com>
2020-02-02 15:11:25 +0800

    Initial Commit

---
+++ vImage_ROI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  vImage_ROI
+//
+//  Created by What on 2020/2/2.
+//  Copyright © 2020 dumbass. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ vImage_ROI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  vImage_ROI
+//
+//  Created by What on 2020/2/2.
+//  Copyright © 2020 dumbass. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
